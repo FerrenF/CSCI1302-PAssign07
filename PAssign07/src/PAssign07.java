8fe9af15acce3feccad52c2f30ee60c777db5997
madmin <madmin@DESKTOP-ET55PR8.intranet.motopc.net>
2022-11-01 11:31:40 -0400

    new commit

---
+++ PAssign07/src/PAssign07.java
@@ -0,0 +1,15 @@
+
+//https://github.com/GSU-CS-1302/csci1302-JavaFX
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.application.*;
+public class PAssign07 extends Application {
+	public void start(Stage primaryStage) {
+// your code here
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
